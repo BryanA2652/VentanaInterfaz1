540cae1aaa2f8d80284c85696e2b25e07bdabe0d
BryanA2652 <bryanayalav.27@gmail.com>
2024-05-04 14:03:58 -0500

    Mi primer repositorio Bryan Ayala

---
+++ src/ventanainterfaz1/FXMLDocumentController.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package ventanainterfaz1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author User
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private ListView <String> Lista;
+ 
+    @FXML
+    private ImageView img1;
+
+    @FXML
+    private ImageView img2;
+
+    @FXML
+    private ImageView img3;
+
+    @FXML
+    private ImageView img4;
+
+    @FXML
+    private ImageView img5;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
+
+    @FXML
+    private Label label4;
+
+    @FXML
+    private Label label5;
+
+    @FXML
+    private TextField txtexto;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO    
+    }    
+    
+}
